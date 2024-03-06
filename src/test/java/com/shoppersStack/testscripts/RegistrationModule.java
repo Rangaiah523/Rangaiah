bbe9bcb8bab3c17194921b0e18a72b7102397623
Anurag Anand <Anurag Anand@TYSS-Anurag-Anand>
2024-03-06 17:13:07 +0530

    1

---
+++ src/test/java/com/shoppersStack/testscripts/RegistrationModule.java
@@ -0,0 +1,10 @@
+package com.shoppersStack.testscripts;
+
+import org.testng.annotations.Listeners;
+
+import com.shoppersStack.generic.BaseClass;
+
+@Listeners(com.shoppersStack.generic.ListenerImplementation.class)
+public class RegistrationModule extends BaseClass {
+
+}
