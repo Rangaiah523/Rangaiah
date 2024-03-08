720e1b20e3d51ebe65e899b4bd4e8fbcd2da2923
Anurag Anand <Anurag Anand@TYSS-Anurag-Anand>
2024-03-08 15:35:57 +0530

    a

--- src/test/java/com/shoppersStack/pom/MyProfilePage.java
+++ src/test/java/com/shoppersStack/pom/MyProfilePage.java
@@ -10,16 +10,35 @@ public class MyProfilePage {
 		PageFactory.initElements(driver, this);
 	}
 
+	// Upload Photo
 	@FindBy(xpath = "(//span[contains(@class,'BaseBadge-badge MuiBadge-badge MuiBadge-standard MuiBadge-anchorOriginBottomRight MuiBadge-anchorOriginBottomRightCircular MuiBadge-overlapCircular css-1mupj9l')])[2]")
-	public WebElement uploadImage_btn;
-	
+	public WebElement uploadPhoto_btn;
+
 	@FindBy(xpath = "//h3[.='Add Profile Photo']")
-	public WebElement addProfilePhoto_btn;
-	
+	public WebElement addProfilePhoto_txt;
+
 	@FindBy(xpath = "//input[@accept=\"image/*\" and @type=\"file\"]")
 	public WebElement chooseFile_btn;
-	
+
 	@FindBy(xpath = "//button[.='upload']")
 	public WebElement upload_btn;
-		
+
+	// Edit Profile
+	@FindBy(xpath = "//button[contains(.,'Edit Profile')]")
+	public WebElement editProfile_btn;
+
+	@FindBy(xpath = "//h2[contains(.,'Edit Profile')]")
+	public WebElement editProfile_txt;
+
+	@FindBy(xpath = "//input[@id='First Name']")
+	public WebElement firstName_txtfield;
+
+	@FindBy(xpath = "//input[@id='Last Name']")
+	public WebElement lastName_txtfield;
+
+	@FindBy(id = "submit")
+	public WebElement submit_btn;
+	
+	@FindBy(id = "City")
+	public WebElement city_dropdown;
 }
--- src/test/java/com/shoppersStack/testscripts/MyProfileModule.java
+++ src/test/java/com/shoppersStack/testscripts/MyProfileModule.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertEquals;
 import java.io.File;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -14,19 +15,43 @@ import com.shoppersStack.generic.BaseClass;
 public class MyProfileModule extends BaseClass {
 
 	@Test(priority = 1)
-	public void uploadProfileImage() throws InterruptedException {
+	public void uploadProfileImage() {
 		new LoginModule().shopperLogin();
 		homePage.accountSettings_btn.click();
 		homePage.myProfile_button.click();
 		wait.until(ExpectedConditions.titleIs("ShoppersStack | Profile"));
 		assertEquals(driver.getTitle(), "ShoppersStack | Profile", "Page title is wrong");
-		myProfilePage.uploadImage_btn.click();
-		myProfilePage.addProfilePhoto_btn.isDisplayed();
+		myProfilePage.uploadPhoto_btn.click();
+		myProfilePage.addProfilePhoto_txt.isDisplayed();
 		File file = new File("src\\test\\resources\\Profile_Image.jpg");
 		myProfilePage.chooseFile_btn.sendKeys(file.getAbsolutePath());
 		wait.until(ExpectedConditions.elementToBeClickable(myProfilePage.upload_btn));
 		myProfilePage.upload_btn.isEnabled();
 		myProfilePage.upload_btn.click();
 		homePage.Success_txt.isDisplayed();
+	}
+
+	@Test(priority = 2)
+	public void editprofile() {
+		new LoginModule().shopperLogin();
+		homePage.accountSettings_btn.click();
+		homePage.myProfile_button.click();
+		wait.until(ExpectedConditions.titleIs("ShoppersStack | Profile"));
+		assertEquals(driver.getTitle(), "ShoppersStack | Profile", "Page title is wrong");
+		myProfilePage.editProfile_btn.click();
+		myProfilePage.editProfile_txt.isDisplayed();
+		myProfilePage.firstName_txtfield.clear();
+		myProfilePage.firstName_txtfield.sendKeys(faker.name().firstName());
+		myProfilePage.lastName_txtfield.clear();
+		myProfilePage.lastName_txtfield.sendKeys(faker.name().lastName());
+		Select city = new Select(myProfilePage.city_dropdown);
+		city.selectByValue("Bengaluru");
+		myProfilePage.submit_btn.click();
+		wait.until(ExpectedConditions.alertIsPresent());
+		String alertMessage = driver.switchTo().alert().getText();
+		assertEquals(alertMessage, "Profile updated", "Alert message is wrong");
+		driver.switchTo().alert().accept();
+		wait.until(ExpectedConditions.titleIs("ShoppersStack | Profile"));
+		assertEquals(driver.getTitle(), "ShoppersStack | Profile", "Page title is wrong");
 	}
 }
