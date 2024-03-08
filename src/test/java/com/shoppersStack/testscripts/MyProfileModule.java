2941d3dc746fd4abb3c3c4c776acf1bfd4d2e698
Anurag Anand <Anurag Anand@TYSS-Anurag-Anand>
2024-03-08 12:23:49 +0530

    anurag

--- src/test/java/com/shoppersStack/testscripts/MyProfileModule.java
+++ src/test/java/com/shoppersStack/testscripts/MyProfileModule.java
@@ -2,6 +2,7 @@ package com.shoppersStack.testscripts;
 
 import static org.testng.Assert.assertEquals;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -21,6 +22,10 @@ public class MyProfileModule extends BaseClass {
 		myProfilePage.uploadImage_btn.click();
 		myProfilePage.addProfilePhoto_btn.isDisplayed();		
 		js.executeScript("document.querySelector(\"body > div.MuiModal-root.css-8ndowl > div.MuiBox-root.css-18zuygp > form > div > input\").click()");
+		
+		String jse = "arguments[0].type='file'";
+		((JavascriptExecutor)driver).executeScript(jse, js);
+		js.sendKeys("absolute_path_of_the_file_to_be_uploaded");
 		
 		myProfilePage.chooseFile_btn.sendKeys("./src/test/resources/Profile_Image.jpg");
 		
